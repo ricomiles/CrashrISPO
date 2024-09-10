d5f5064da84acdec30168c7c622f2ebdfc0a7bd6
Rico Miles <ricoquiblat@gmail.com>
2024-09-11 03:09:47 +0800

    removed temp.txt

--- obj/Debug/net8.0/CrashrISPO.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrashrISPO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrashrISPO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0cd074205a4f82cc8e8f2ec85e9d360265f3017")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrashrISPO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrashrISPO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
